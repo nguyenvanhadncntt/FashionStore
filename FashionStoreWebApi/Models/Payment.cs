c451d070470bf19626dd84890ba1315d40120dcb
nguyenvanhadncntt <nguyenvanhadncntt@gmail.com>
2025-02-13 01:50:16 +0700

    update Payment entity

--- FashionStoreWebApi/Models/Payment.cs
+++ FashionStoreWebApi/Models/Payment.cs
@@ -1,12 +1,14 @@
-﻿namespace FashionStoreWebApi.Models
+﻿using FashionStoreWebApi.Models.Enumerations;
+
+namespace FashionStoreWebApi.Models
 {
     public class Payment
     {
         public long Id { get; set; }
         public long OrderId { get; set; }
         public decimal Amount { get; set; }
-        public string PaymentStatus { get; set; } // Success/Failure/Pending
-        public string PaymentMethod { get; set; }
+        public PaymentStatus PaymentStatus { get; set; } // Success/Failure/Pending
+        public PaymentMethod PaymentMethod { get; set; }
         public string TransactionId { get; set; }
         public DateTime CreatedAt { get; set; }
 
